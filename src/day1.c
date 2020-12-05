14a2b79964e831c6c735f4a2a77c2ba8af0ec0fb
Lewis Catley <lewis@catley.me>
2020-12-05 15:52:55 +0000

    Solve day 1 part 2 with recursion

--- src/day1.c
+++ src/day1.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 // Need to store the length of the array with the array
 // This is because it's a dynamic array
@@ -9,40 +10,49 @@ typedef struct Input {
 } Inp;
 
 Inp readFile(char *);
-int * findSumValues(Inp);
+void findRecursive(Inp, int *, int, int, int);
+bool arrValuesMatchSum(int *, int, int);
 
 int main() {
 	Inp values = readFile("./input/day1.1.txt");
-	int *answer = findSumValues(values);
-	printf("The final answer to day one is: %d\n", (answer[0] * answer[1]));
+	int * part1 = (int *)malloc(2 * sizeof(int));
+	findRecursive(values, part1, 2020, 1, 0);
+	printf("The final answers to part one is: %d and %d\n", part1[0], part1[1]);
+	printf("The final answer to part one is: %d\n", (part1[0] * part1[1]));
+
+	int * part2 = (int *)malloc(3 * sizeof(int));
+	findRecursive(values, part2, 2020, 2, 0);
+	printf("The final answers to part two is: %d, %d %d\n", part2[0], part2[1], part2[2]);
+	printf("The final answer to part two one is: %d\n", (part2[0] * part2[1] * part2[2]));
 }
 
-// A sub-routine to get the sum values that add up to 2020.
-int * findSumValues(Inp values) {
-	// Declare out we only return 2 values so we know the size of the array
-	static int out[2] = {0};
-	int i, j;
+bool arrValuesMatchSum(int * arr, int length, int sum) {
+	int val = 0;
+	for (int i = 0; i <= length; i++) {
+		// We don't want to accidently return true if the array contains 0's
+		if (arr[i] == 0)
+			return false;
+		val += arr[i];
+	}
+	return (val == sum);
+}
 
-	for (i = 0; i < values.length; i++) {
-		for (j = 0; j < values.length; j++) {
-			// Has to be 2 separate entries 
-			if (j == i) 
-				continue;
-			// Break to the parent for loop
-			if (values.array[i] + values.array[j] == 2020)
-				break;
+void findRecursive(Inp values, int * out, int sum, int depth, int currDepth) {
+	for (int i = 0; i < values.length; i++) {
+		// We have what we want now retreat
+		if (arrValuesMatchSum(out, depth, sum)) {
+			return;
 		}
-		// Double check that the values add to 2020
-		// Set the values and return
-		if (values.array[i] + values.array[j] == 2020) {
-			printf("%d + %d = 2020 \n", values.array[i], values.array[j]);
-			out[0] = values.array[i];
-			out[1] = values.array[j];
-			break;
+		out[currDepth] = values.array[i];
+		if (depth == currDepth) {
+			// Keep looping through the values;
+			continue;
+		} else {
+			// We're not at the bottom depth call recursively?
+			findRecursive(values, out, sum, depth, currDepth + 1);
 		}
 	}
-
-	return out;
+	return;
 }
 
 Inp readFile(char * fileName) {
